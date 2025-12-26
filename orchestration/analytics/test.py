708cf384a49a5d8c336438a521ab912a2d6fd9a7
nguyenyenchi <ycn821@outlook.com>
2025-12-26 17:58:43 +1100

    check python lint

--- orchestration/analytics/test.py
+++ orchestration/analytics/test.py
@@ -1,1 +1,11 @@
-print("no workflow triggered v3    ")
+import pandas as pd
+from datetime import datetime
+import re
+import numpy as np
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
