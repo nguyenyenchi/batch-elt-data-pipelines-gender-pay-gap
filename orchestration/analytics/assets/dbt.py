e2b1b6389c695c52a506974c22048e2dd05f6ef0
nguyenyenchi <ycn821@outlook.com>
2025-12-27 21:35:06 +1100

    add preprocess assets and set dbt target

--- orchestration/analytics/assets/dbt.py
+++ orchestration/analytics/assets/dbt.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 
-from dagster import AssetExecutionContext, AutomationCondition
+from dagster import AssetExecutionContext, AutomationCondition, EnvVar
 from dagster_dbt import DagsterDbtTranslator, DbtCliResource, dbt_assets
 
 # construct relative path to the dbt project directory
 # dbt_project_dir = Path(__file__).joinpath("..","..","..", "..","..","transformation", "dw").resolve() # current_file_path -> navigate to dbt_project folder
 # dbt_warehouse_resource: DbtCliResource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
-dbt_warehouse_resource: DbtCliResource = DbtCliResource(project_dir="transformation/dw")
+# Set dbt target
+
+
+BRANCH = EnvVar("GIT_BRANCH", "local")      # injected by CI/CD
+DBT_TARGET = "prod" if BRANCH == "main" else "dev"
+
+dbt_warehouse_resource: DbtCliResource = DbtCliResource(project_dir="transformation/dw", target=DBT_TARGET)
 
 # Install dbt packages
 deps_res = dbt_warehouse_resource.cli(["deps"], target_path=Path("target")).wait()
--- orchestration/analytics/assets/preprocess.py
+++ orchestration/analytics/assets/preprocess.py
@@ -1,10 +1,12 @@
 import boto3
+from dagster import asset
 
+@asset()
 def upload_to_s3():
     s3 = boto3.client("s3")
 
     s3.upload_file(
-        Filename="../data/S3/EEH_2025_s3_test.csv",
+        Filename="analytics/data/EEH_2025_s3_test.csv",
         Bucket="eeh-files",
         Key='eeh/EEH_2025_s3_test.csv'
     )
