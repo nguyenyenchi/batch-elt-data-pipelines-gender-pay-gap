07df4522c07fa56135717c2b1c10a50504a81d01
nguyenyenchi <ycn821@outlook.com>
2025-12-28 18:39:31 +1100

    change file location

--- orchestration/analytics/assets/preprocess.py
+++ orchestration/analytics/assets/preprocess.py
@@ -8,7 +8,8 @@ def upload_to_s3():
     s3 = boto3.client("s3")
 
     s3.upload_file(
-        Filename="analytics/data/EEH_2025_s3_test.csv",
+        # Filename="analytics/data/EEH_2025_s3_test.csv",
+        Filename="data/EEH_2025_s3_test.csv",
         Bucket="eeh-files",
         Key='eeh/EEH_2025_s3_test.csv'
     )
